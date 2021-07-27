8a8d8e342b30ca9b172bf1cbedfc97e667f2aaad
yueh feng <jason70160@gmail.com>
2021-07-27 21:32:12 +0800

    first commit

---
+++ products01.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+	name = input('請輸入商品名稱')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格')
+	d = [name, price]
+	products.append(d)
+print(products)
+for p in products:
+	print(p[0], '價格是', p[1])
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
